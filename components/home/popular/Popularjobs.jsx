784f89d3322acedf9625a281752b2bcb23832fca
ahmed nasser <ahmednas197@gmail.com>
2023-06-05 16:21:42 +0300

    Added fetch hook and popular jobs card

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -10,11 +10,16 @@ import { useRouter } from "expo-router";
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "./../../common/cards/popular/PopularJobCard";
+import useFetch from "./../../../hook/useFetch";
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+  const { data, isLoading, error, refetch } = useFetch("search", {
+    query: "React Developer",
+    num_pages: 1,
+  });
+
+  // console.log("data", data);
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -31,7 +36,7 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4]}
+            data={data}
             renderItem={({ item }) => <PopularJobCard item={item} />}
             keyExtractor={(item) => item?.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
